2b2d953f5d94f267c9c721319f943e96c2ba1339
Pasang Lama <snowpasang@gmail.com>
2026-07-01 14:39:29 +0800

    added donation, analytics and mealplan views and serializers, full auth flow working

--- smartpantry_backend/analytics/views.py
+++ smartpantry_backend/analytics/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from food.models import FoodItem
+from donation.models import Donation
 
-# Create your views here.
+class AnalyticsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Count total food items
+        total_items = FoodItem.objects.filter(
+            user=request.user
+        ).count()
+
+        # Count donated items
+        total_donated = Donation.objects.filter(
+            donor=request.user
+        ).count()
+
+        # Count expiring soon items
+        from django.utils import timezone
+        from datetime import timedelta
+        soon = timezone.now().date() + timedelta(days=3)
+        expiring_soon = FoodItem.objects.filter(
+            user=request.user,
+            expiry_date__lte=soon
+        ).count()
+
+        return Response({
+            'total_items': total_items,
+            'total_donated': total_donated,
+            'expiring_soon': expiring_soon,
+        })
--- smartpantry_backend/mealplan/views.py
+++ smartpantry_backend/mealplan/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import MealPlan
+from .serializers import MealPlanSerializer
 
-# Create your views here.
+class MealPlanViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MealPlanSerializer
+
+    def get_queryset(self):
+        # Users can only see their own meal plans
+        return MealPlan.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically set the user when creating a meal plan
+        serializer.save(user=self.request.user)
