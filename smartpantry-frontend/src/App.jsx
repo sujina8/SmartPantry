3386bf26836dbf9fc737c7b9ca87f4854a954197
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-01 18:36:49 +0800

    added nav bar with navigation link

--- smartpantry-frontend/src/App.jsx
+++ smartpantry-frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './context/AuthContext'
+import Navbar from './components/Navbar'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Inventory from './pages/Inventory'
-
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth()
@@ -16,21 +16,24 @@ const ProtectedRoute = ({ children }) => {
 
 function App() {
   return (
-    <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/dashboard" element={
-    <ProtectedRoute>
-      <Dashboard />
-    </ProtectedRoute>
-  } />
-  <Route path="/inventory" element={
-    <ProtectedRoute>
-      <Inventory />
-    </ProtectedRoute>
-  } />
-</Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/inventory" element={
+          <ProtectedRoute>
+            <Inventory />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </>
   )
 }
 
