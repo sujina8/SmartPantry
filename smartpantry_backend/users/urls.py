fc915e50707fc26e8a62a2367e39f4533b6df3ae
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-19 22:32:47 +0800

    feat: add OTP authentication routes

--- smartpantry_backend/users/urls.py
+++ smartpantry_backend/users/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('verify-otp/', views.VerifyOTPView.as_view(), name='verify_otp'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
