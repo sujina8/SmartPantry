4775a16e9a909270a4b86bc1ee5f7052b391ed93
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-20 21:45:01 +0800

    feat: add email verification routes

--- smartpantry_backend/users/urls.py
+++ smartpantry_backend/users/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('register/', views.RegisterView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('verify-otp/', views.VerifyOTPView.as_view(), name='verify_otp'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('resend-code/', views.resend_code, name='resend_code'),
 ]
