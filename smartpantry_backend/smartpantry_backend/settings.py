1f6609578f5c6b88fbf3a542ebe807085cf353b8
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-20 21:45:25 +0800

    config: enable Gmail SMTP for OTP emails

--- smartpantry_backend/smartpantry_backend/settings.py
+++ smartpantry_backend/smartpantry_backend/settings.py
@@ -122,4 +122,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings (for OTP and notifications)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
