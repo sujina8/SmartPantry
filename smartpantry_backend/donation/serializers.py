deeb284d7b273e03dfa5b4d08aa9d5b0b2db49dc
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-22 11:12:17 +0800

    feat: update donation serializer

--- smartpantry_backend/donation/serializers.py
+++ smartpantry_backend/donation/serializers.py
@@ -1,15 +1,16 @@
 from rest_framework import serializers
 from .models import Donation
+from food.serializers import FoodItemSerializer
 
 class DonationSerializer(serializers.ModelSerializer):
     donor_email = serializers.ReadOnlyField(source='donor.email')
-    food_item_name = serializers.ReadOnlyField(source='food_item.name')
+    food_item_detail = FoodItemSerializer(source='food_item', read_only=True)
 
     class Meta:
         model = Donation
         fields = [
             'id', 'donor_email', 'food_item',
-            'food_item_name', 'description',
+            'food_item_detail', 'description',
             'pickup_info', 'status', 'created_at'
         ]
         read_only_fields = ['id', 'status', 'created_at', 'donor_email']
