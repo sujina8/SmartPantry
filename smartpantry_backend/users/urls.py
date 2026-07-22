e345e6b06970b2319da1d45b6607090421bac032
sujina.tuladhar <sujina.tuladhar23@midvalley.edu.np>
2026-07-22 11:12:19 +0800

    feat: update user routes

--- smartpantry_backend/users/urls.py
+++ smartpantry_backend/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from .views import UserSettingsView
 
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('verify-otp/', views.verify_otp, name='verify_otp'),
     path('resend-code/', views.resend_code, name='resend_code'),
+    path("settings/", UserSettingsView.as_view(), name="user-settings"),
 ]
